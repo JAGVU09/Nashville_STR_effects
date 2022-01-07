6a6d4c7280b0802db04870a68a5b854a67a9124f
James Gilbert <jamesgilbert@Jamess-MacBook-Air.local>
2022-01-06 21:11:13 -0600

    Messed around with geoJSON and broke API call, changed back and will convert JSON to geoJSON as soon as i figure out how to

---
+++ NashvilleSTRs/global.R
@@ -0,0 +1,29 @@
+library(httr)
+library(tidyverse)
+library(jsonlite)
+library(sf)
+library(leaflet)
+
+url = 'https://data.nashville.gov/resource/479w-kw2x.json'
+
+query = list(
+  '$select'= 'request,date_received,property_apn,property_address,reported_problem,council_district,mapped_location',
+  '$limit' = 90000
+)
+
+response <- GET(url, query=query )
+
+Violations <- content(response, as = 'text') %>% 
+  fromJSON()
+
+url = 'https://data.nashville.gov/resource/2z82-v8pm.json'
+
+query = list(
+  '$select'= 'permit, permit_status, parcel, address, council_dist, mapped_location',
+  '$limit' = 13000
+)
+
+response <- GET(url, query = query)
+
+STRs <- content(response, as = 'text') %>% 
+  fromJSON()
