821efaace09b407596495ff90c674322ac29ca52
James Gilbert <jamesgilbert@Jamess-MacBook-Air.local>
2022-01-18 21:41:06 -0600

    Got some reactivity working sort of. but the map is not responding by recoloring the map. and the heat map is broken on all the selections

--- NashvilleSTRs/global.R
+++ NashvilleSTRs/global.R
@@ -62,18 +62,4 @@ grouped_viol<-Violations %>%
 STRs_Viol_per_district<-merge(grouped_STRs, grouped_viol, by = 'council_district') %>% 
   mutate(council_district = as.numeric(council_district))
 
-STRbins <- c(0,10,20,100,200,400,700,1100,1600,2200,2900,3700,4600, 5600, 6700)
-STRbinpal <- colorBin(heat.colors(15), domain = STRs_Viol_per_district$STRs_per_dist, bins = STRbins, reverse = TRUE)
 
-STRlabels <- sprintf(
-  "<strong>%s</strong><br/>%g STRs / mi<sup>2</sup>",
-  STRs_Viol_per_district$council_dist, STRs_Viol_per_district$STRs_per_dist
-) %>% lapply(htmltools::HTML)
-
-Violbins <- c(0,100,200,400,700,1100,1600,2200,2900,3700,4600, 5600, 6700)
-Violbinpal <- colorBin(heat.colors(12), domain = STRs_Viol_per_district$Violations_per_dist, bins = Violbins)
-
-Viollabels <- sprintf(
-  "<strong>%s</strong><br/>%g Violations / mi<sup>2</sup>",
-  grouped_viol$council_dist, grouped_viol$Violations_per_dist
-) %>% lapply(htmltools::HTML)
--- NashvilleSTRs/server.R
+++ NashvilleSTRs/server.R
@@ -11,20 +11,25 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
+  filteredData<- reactive({
+    STRs_Viol_per_district 
+  })  
+  colorpal<-reactive({
+    colorBin(heat.colors(14), domain = input$strscodes, reverse = TRUE)
+  })
+  labels<-reactive({
+    sprintf(
+      "<strong>%s</strong><br/>%s STRs / mi<sup>2</sup>",
+      STRs_Viol_per_district$council_dist, input$strscodes
+    ) %>% lapply(htmltools::HTML) 
+  }) 
+  
   output$map<-renderLeaflet({
-    STRbinpal <- colorBin(heat.colors(14), domain = STRs_Viol_per_district$STRs_per_dist, reverse = TRUE)
-    
-    STRlabels <- sprintf(
-      "<strong>%s</strong><br/>%g STRs / mi<sup>2</sup>",
-      STRs_Viol_per_district$council_dist, STRs_Viol_per_district$STRs_per_dist
-    ) %>% lapply(htmltools::HTML)
-    
-    
-    STRmap<- leaflet(data = STRs_Viol_per_district, options = leafletOptions(minZoom = 0, maxZoom = 20)) %>% 
+    STRmap<- leaflet(data = filteredData(), options = leafletOptions(minZoom = 0, maxZoom = 20)) %>% 
       addTiles() %>% addProviderTiles(providers$Stamen.TonerLite,
                                       options = providerTileOptions(noWrap = TRUE)) %>% 
       addPolygons(data = council_districts,
-                  fillColor = ~STRbinpal(STRs_Viol_per_district$STRs_per_dist),
+                  fillColor = colorpal(),
                   opacity = 0.2,
                   dashArray = "3",
                   fillOpacity = 0.9,
@@ -34,7 +39,7 @@ shinyServer(function(input, output) {
                     dashArray = "",
                     fillOpacity = 0.7,
                     bringToFront = TRUE),
-                  label = STRlabels,
+                  label = labels(),
                   labelOptions = labelOptions(
                     style = list('font-weight' = 'normal', padding = '3px 8px'),
                     textsize = '15px',
@@ -54,6 +59,6 @@ shinyServer(function(input, output) {
   output$correlation <- renderText({
     correlation <- cor(STRs_Viol_per_district$STRs_per_dist, STRs_Viol_per_district$Violations_per_dist)%>% round(2)
     paste0('<b>Correlation between Short Term Rentals and Code Violations:</b> ', correlation)
-})
+  })
 })
 
